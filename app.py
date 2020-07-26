4ca4956e15af1b4f6922dbac4e9ddfa17d6cc859
Andy <apaul@posteo.de>
2020-07-26 23:39:59 +0400

    removed import: plot.ly

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import datetime
 import numpy as np
 import pandas as pd
 import streamlit as st
-import plotly.express as px
 import matplotlib.pyplot as plt
 from streamlit_folium import folium_static
 
